ddc62d6b7139ea799a2d4f86276bd92fe505d1bb
Mousa <mousasalib601@gmail.com>
2023-10-06 22:38:24 +0300

    i have created the function of addProduct, the function of delete, save data in local storage, and i have modified in the design of products

--- lab1/src/components/Child/Child.jsx
+++ lab1/src/components/Child/Child.jsx
@@ -5,12 +5,13 @@ export default class Child extends Component {
   render() {
     let {name, price} = this.props.productDetails
     return (
-      <div className='col-md-4'>
-        <div className='container bg-success p-2'>
-        <h2>Child works</h2>
+      <div className='col-md-4 p-1'>
+        <div className='container bg-warning'>
+          <h2>Child works</h2>
           <h5>Name: {name}</h5>
           <h5>Price: {price}</h5>
-          <button className='btn btn-info' onClick={() => this.props.upDatePrice(this.props.index)}>Increase</button>
+          <button className='btn btn-info m-2' onClick={() => this.props.upDatePrice(this.props.index)}>Increase</button>
+          <button className='btn btn-danger m-2' onClick={() => this.props.deleteProduct(this.props.index)}>Delete</button>
         </div>
       </div>
     )
--- lab1/src/components/Parent/Parent.jsx
+++ lab1/src/components/Parent/Parent.jsx
@@ -21,21 +21,90 @@ export default class Parent extends Component {
         name: "Html",
         price: 250,
         id: 3
+      },
+      {
+        name: "Bootstrap",
+        price: 250,
+        id: 4
+      },
+      {
+        name: "Angular",
+        price: 750,
+        id: 5
+      },
+      {
+        name: "React",
+        price: 800,
+        id: 6
+      },
+      {
+        name: "Node Js",
+        price: 1000,
+        id: 7
+      },
+      {
+        name: "Express",
+        price: 200,
+        id: 8
+      },
+      {
+        name: "Sass",
+        price: 250,
+        id: 9
+      },
+      {
+        name: "Jquery",
+        price: 300,
+        id: 10
       }
     ]
   }
+
+  addProduct = () => {
+    const newProduct = {
+      name: "anyThing",
+      price: 0,
+      id: this.state.products.length + 1
+    };
+
+    this.setState({products: [...this.state.products, newProduct]})
+  };
+  
   upDatePrice = (index) => {
-    console.log(index)
+    // console.log(index)
     let products = [...this.state.products];
     products[index].price += 10;
     this.setState({products: products})
+  };
+
+  deleteProduct = (index) => {
+    let products = [...this.state.products];
+    products.splice(index, 1);
+    this.setState({products: products})
+  };
+
+  saveDataInLocalStorage = () => {
+    localStorage.setItem('products', JSON.stringify(this.state.products));
   }
+
+  componentDidMount() {
+    const savedProducts = localStorage.getItem('products');
+    if (savedProducts) {
+      this.setState({ products: JSON.parse(savedProducts) });
+    }
+  }
+
+  componentDidUpdate() {
+    this.saveDataInLocalStorage()
+  }
+
   render() {
     return (
       <div className='container bg-info'>
         <div className='row p-5'>
           <h1>Parent works!</h1>
-          {this.state.products.map((product, index) => <Child key={index} productDetails= {product} upDatePrice = {this.upDatePrice} index={index}/>)}
+          <button className='btn btn-success m-2' onClick={this.addProduct}>Add Product</button>
+          {this.state.products.map((product, index) => <Child key={index} productDetails= {product} upDatePrice = {this.upDatePrice} index={index} deleteProduct = {this.deleteProduct} />)}
         </div>
       
       </div>
